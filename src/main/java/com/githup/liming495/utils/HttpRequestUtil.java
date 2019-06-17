45458093f25e069c4aca80298e68b033db26dd1c
wop129@tom.com <wop129@tom.com>
2019-06-17 18:10:31 +0800

    modify

---
+++ src/main/java/com/githup/liming495/utils/HttpRequestUtil.java
@@ -0,0 +1,139 @@
+package com.githup.liming495.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * HTTP请求工具
+ *
+ * @author Guppy
+ */
+public class HttpRequestUtil {
+    /**
+     * 向指定URL发送GET方法的请求
+     *
+     * @param url   发送请求的URL
+     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+     * @return URL 所代表远程资源的响应结果
+     */
+    public static String sendGet(String url, Map<String, String> headerParam, String param) {
+        String result = "";
+        BufferedReader in = null;
+        try {
+            String urlNameString;
+            if(!ObjectUtils.isEmpty(param)) {
+                urlNameString = url + "?" + param;
+            } else {
+                urlNameString = url;
+            }
+            // 打开和URL之间的连接
+            URLConnection conn = getConn(urlNameString, headerParam);
+
+            // 建立实际的连接
+            conn.connect();
+            // 获取所有响应头字段
+            Map<String, List<String>> map = conn.getHeaderFields();
+            // 遍历所有的响应头字段
+            for (String key : map.keySet()) {
+                System.out.println(key + "--->" + map.get(key));
+            }
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(
+                    conn.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送GET请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        // 使用finally块来关闭输入流
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        System.out.println("GET推送结果：" + result);
+        return result;
+    }
+
+    /**
+     * 向指定 URL 发送POST方法的请求
+     *
+     * @param url   发送请求的 URL
+     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+     * @return 所代表远程资源的响应结果
+     */
+    public static String sendPost(String url, Map<String, String> headerParam, String param) {
+        PrintWriter out = null;
+        BufferedReader in = null;
+        String result = "";
+        try {
+            // 打开和URL之间的连接
+            URLConnection conn = getConn(url, headerParam);
+            // 发送POST请求必须设置如下两行
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            if (!ObjectUtils.isEmpty(param)) {
+                System.out.println(param);
+                //1.获取URLConnection对象对应的输出流
+                out = new PrintWriter(conn.getOutputStream());
+                //2.中文有乱码的需要将PrintWriter改为如下
+                //out=new OutputStreamWriter(conn.getOutputStream(),"UTF-8")
+                // 发送请求参数
+                out.print(param);
+                // flush输出流的缓冲
+                out.flush();
+            }
+
+            // 定义BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送 POST 请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        //使用finally块来关闭输出流、输入流
+        finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        System.out.println("post推送结果：" + result);
+        return result;
+    }
+
+    private static URLConnection getConn(String url, Map<String, String> headerParam) throws IOException {
+        URL realUrl = new URL(url);
+        // 打开和URL之间的连接
+        URLConnection conn = realUrl.openConnection();
+        // 设置通用的请求属性
+        if (!ObjectUtils.isEmpty(headerParam)) {
+            for (Map.Entry<String, String> entry : headerParam.entrySet()) {
+                conn.setRequestProperty(entry.getKey(), entry.getValue());
+            }
+        }
+        return conn;
+    }
+}
