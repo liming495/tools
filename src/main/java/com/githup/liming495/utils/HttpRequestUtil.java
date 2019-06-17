36fde09a5e558da2dbc3cf585e32ca45076aa18d
wop129@tom.com <wop129@tom.com>
2019-06-17 18:12:00 +0800

    modify

--- src/main/java/com/githup/liming495/utils/HttpRequestUtil.java
+++ src/main/java/com/githup/liming495/utils/HttpRequestUtil.java
@@ -19,7 +19,7 @@ public class HttpRequestUtil {
      * 向指定URL发送GET方法的请求
      *
      * @param url   发送请求的URL
-     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+     * @param param 请求参数，请求参数应该是 <p>name1=value1&name2=value2</p> 的形式。
      * @return URL 所代表远程资源的响应结果
      */
     public static String sendGet(String url, Map<String, String> headerParam, String param) {
@@ -72,7 +72,7 @@ public class HttpRequestUtil {
      * 向指定 URL 发送POST方法的请求
      *
      * @param url   发送请求的 URL
-     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+     * @param param 请求参数，请求参数应该是 <p>name1=value1&name2=value2</> 的形式。
      * @return 所代表远程资源的响应结果
      */
     public static String sendPost(String url, Map<String, String> headerParam, String param) {
