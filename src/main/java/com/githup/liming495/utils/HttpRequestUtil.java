8b5badf669d3a1fbbd6812b8c1495004d90e08eb
wop129@tom.com <wop129@tom.com>
2019-06-17 18:15:24 +0800

    modify

--- src/main/java/com/githup/liming495/utils/HttpRequestUtil.java
+++ src/main/java/com/githup/liming495/utils/HttpRequestUtil.java
@@ -19,6 +19,7 @@ public class HttpRequestUtil {
      * 向指定URL发送GET方法的请求
      *
      * @param url   发送请求的URL
+     * @param headerParam   发送请求的Header参数
      * @param param 请求参数，请求参数应该是的形式。
      * @return URL 所代表远程资源的响应结果
      */
@@ -72,6 +73,7 @@ public class HttpRequestUtil {
      * 向指定 URL 发送POST方法的请求
      *
      * @param url   发送请求的 URL
+     * @param headerParam   发送请求的Header参数
      * @param param 请求参数，请求参数应该是的形式。
      * @return 所代表远程资源的响应结果
      */
