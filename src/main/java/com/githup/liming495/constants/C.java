68cd939338aee5ff9d1c255f0a437420be810f15
wop129@tom.com <wop129@tom.com>
2019-07-17 13:11:08 +0800

    modify file name

---
+++ src/main/java/com/githup/liming495/constants/C.java
@@ -0,0 +1,11 @@
+package com.githup.liming495.constants;
+
+/**
+ * 常量
+ *
+ * @author Guppy
+ */
+public class C {
+    public static String STATUS_0 = "0";    //状态:0
+    public static String STATUS_1 = "1";    //状态:1
+}
