a13029cda9dc942856d4cbe56cc0e005aa796367
wop129@tom.com <wop129@tom.com>
2019-08-07 16:56:27 +0800

    add ip tools

---
+++ src/main/java/com/githup/liming495/utils/IPUtils.java
@@ -0,0 +1,35 @@
+package com.githup.liming495.utils;
+
+/**
+ * 互联网协议地址工具类
+ *
+ * @author Guppy
+ */
+public class IPUtils {
+
+    /**
+     * 判断互联网协议地址是否在网段内
+     *
+     * @param ip   互联网协议地址
+     * @param cidr 网段
+     * @return 结果
+     */
+    public static boolean isInRange(String ip, String cidr) {
+        String[] ips = ip.split("\\.");
+        int ipAddr = (Integer.parseInt(ips[0]) << 24)
+                | (Integer.parseInt(ips[1]) << 16)
+                | (Integer.parseInt(ips[2]) << 8) | Integer.parseInt(ips[3]);
+        int type = Integer.parseInt(cidr.replaceAll(".*/", ""));
+        int mask = 0xFFFFFFFF << (32 - type);
+        String cidrIp = cidr.replaceAll("/.*", "");
+        String[] cidrIps = cidrIp.split("\\.");
+        int cidrIpAddr = (Integer.parseInt(cidrIps[0]) << 24)
+                | (Integer.parseInt(cidrIps[1]) << 16)
+                | (Integer.parseInt(cidrIps[2]) << 8)
+                | Integer.parseInt(cidrIps[3]);
+
+        return (ipAddr & mask) == (cidrIpAddr & mask);
+
+    }
+
+}
