88d974d252259d3467bd99baf7bff5154dd4d1e6
wop129@tom.com <wop129@tom.com>
2019-09-29 10:58:24 +0800

    add getHeaderFields

--- src/main/java/com/githup/liming495/utils/HttpRequestUtil.java
+++ src/main/java/com/githup/liming495/utils/HttpRequestUtil.java
@@ -25,7 +25,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
