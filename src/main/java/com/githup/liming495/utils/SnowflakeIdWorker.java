96b345c1bc4c74158439a84d9f641351e66b7d2b
eidc <liming_ly@126.com>
2021-01-13 18:50:09 +0800

    upgrade 1.1.6

---
+++ src/main/java/com/githup/liming495/utils/SnowflakeIdWorker.java
@@ -0,0 +1,190 @@
+package com.githup.liming495.utils;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+import java.util.Random;
+
+/**
+ * Twitter_Snowflake<br>
+ * SnowFlake的结构如下(每部分用-分开):<br>
+ * 0 - 0000000000 0000000000 0000000000 0000000000 0 - 00000 - 00000 - 000000000000 <br>
+ * 1位标识，由于long基本类型在Java中是带符号的，最高位是符号位，正数是0，负数是1，所以id一般是正数，最高位是0<br>
+ * 41位时间截(毫秒级)，注意，41位时间截不是存储当前时间的时间截，而是存储时间截的差值（当前时间截 - 开始时间截)
+ * 得到的值），这里的的开始时间截，一般是我们的id生成器开始使用的时间，由我们程序来指定的（如下下面程序IdWorker类的startTime属性）。41位的时间截，可以使用69年，年T = (1L << 41) / (1000L * 60 * 60 * 24 * 365) = 69<br>
+ * 10位的数据机器位，可以部署在1024个节点，包括5位dataCenterId和5位workerId<br>
+ * 12位序列，毫秒内的计数，12位的计数顺序号支持每个节点每毫秒(同一机器，同一时间截)产生4096个ID序号<br>
+ * 加起来刚好64位，为一个Long型。<br>
+ * SnowFlake的优点是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，并且效率较高，经测试，SnowFlake每秒能够产生26万ID左右。
+ *
+ * @author guppy
+ * @since 2021/1/13 18:26
+ */
+public class SnowflakeIdWorker {
+    private final static long TWEPOCH = 1288834974657L;
+
+    // 机器标识位数
+    private final static long WORKER_ID_BITS = 5L;
+
+    // 数据中心标识位数
+    private final static long DATA_CENTER_ID_BITS = 5L;
+
+    // 机器ID最大值 31
+    private final static long MAX_WORKER_ID = -1L ^ (-1L << WORKER_ID_BITS);
+
+    // 数据中心ID最大值 31
+    private final static long MAX_DATA_CENTER_ID = -1L ^ (-1L << DATA_CENTER_ID_BITS);
+
+    // 毫秒内自增位
+    private final static long SEQUENCE_BITS = 12L;
+
+    // 机器ID偏左移12位
+    private final static long WORKER_ID_SHIFT = SEQUENCE_BITS;
+
+    private final static long DATA_CENTER_ID_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS;
+
+    // 时间毫秒左移22位
+    private final static long TIMESTAMP_LEFT_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS + DATA_CENTER_ID_BITS;
+
+    private final static long SEQUENCE_MASK = -1L ^ (-1L << SEQUENCE_BITS);
+
+    private long lastTimestamp = -1L;
+
+    private long sequence = 0L;
+    private final long workerId;
+    private final long dataCenterId;
+    private static volatile SnowflakeIdWorker snowflake = null;
+    private static Object lock = new Object();
+
+    //单例禁止new实例化
+    private SnowflakeIdWorker(long workerId, long dataCenterId) {
+        if (workerId > MAX_WORKER_ID || workerId < 0) {
+            workerId = getRandom();
+        }
+
+        if (dataCenterId > MAX_DATA_CENTER_ID || dataCenterId < 0) {
+
+            throw new IllegalArgumentException(String.format("%s 数据中心ID最大值 必须是 %d 到 %d 之间", dataCenterId, 0, MAX_DATA_CENTER_ID));
+        }
+        this.workerId = workerId;
+        this.dataCenterId = dataCenterId;
+    }
+
+    /**
+     * 获取单列
+     *
+     * @return
+     */
+    public static SnowflakeIdWorker getInstanceSnowflake() {
+        if (snowflake == null) {
+            synchronized (lock) {
+                long workerId ;
+                long dataCenterId = getRandom();
+                try {
+                    //第一次使用获取mac地址的
+                    workerId = getWorkerId();
+                } catch (Exception e) {
+                    workerId = getRandom();
+                }
+                snowflake = new SnowflakeIdWorker(workerId, dataCenterId);
+            }
+        }
+        return snowflake;
+    }
+
+    /**
+     * 生成1-31之间的随机数
+     *
+     * @return
+     */
+    private static long getRandom() {
+        int max = (int) (MAX_WORKER_ID);
+        int min = 1;
+        Random random = new Random();
+        long result = random.nextInt(max - min) + min;
+        return result;
+    }
+
+    public static String getSnowflakeId() throws Exception{
+        return SnowflakeIdWorker.getInstanceSnowflake().nextId()+"";
+    }
+
+    private synchronized long nextId() throws Exception {
+        long timestamp = time();
+        if (timestamp < lastTimestamp) {
+            throw new Exception("时钟向后移动，拒绝生成id  " + (lastTimestamp - timestamp) + " milliseconds");
+        }
+
+        if (lastTimestamp == timestamp) {
+            // 当前毫秒内，则+1
+            sequence = (sequence + 1) & SEQUENCE_MASK;
+            if (sequence == 0) {
+                // 当前毫秒内计数满了，则等待下一秒
+                timestamp = tilNextMillis(lastTimestamp);
+            }
+        } else {
+            sequence = 0;
+        }
+        lastTimestamp = timestamp;
+
+        // ID偏移组合生成最终的ID，并返回ID
+        long nextId = ((timestamp - TWEPOCH) << TIMESTAMP_LEFT_SHIFT)
+                | (dataCenterId << DATA_CENTER_ID_SHIFT) | (workerId << WORKER_ID_SHIFT) | sequence;
+
+        return nextId;
+    }
+
+    private long tilNextMillis(final long lastTimestamp) {
+        long timestamp = this.time();
+        while (timestamp <= lastTimestamp) {
+            timestamp = this.time();
+        }
+        return timestamp;
+    }
+
+    private long time() {
+        return System.currentTimeMillis();
+    }
+
+    @SuppressWarnings("Duplicates")
+    private static long getWorkerId() throws SocketException, UnknownHostException, NullPointerException {
+        @SuppressWarnings("unused")
+        InetAddress ip = InetAddress.getLocalHost();
+
+        NetworkInterface network = null;
+        Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+        while (en.hasMoreElements()) {
+            NetworkInterface nint = en.nextElement();
+            if (!nint.isLoopback() && nint.getHardwareAddress() != null) {
+                network = nint;
+                break;
+            }
+        }
+
+        @SuppressWarnings("ConstantConditions")
+        byte[] mac = network.getHardwareAddress();
+
+        Random rnd = new Random();
+        byte rndByte = (byte) (rnd.nextInt() & 0x000000FF);
+
+        // 取mac地址最后一位和随机数
+        return ((0x000000FF & (long) mac[mac.length - 1]) | (0x0000FF00 & (((long) rndByte) << 8))) >> 6;
+    }
+
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        try {
+            for (int i = 0, len = 100000; i < len; i++) {
+                //getSnowflakeId();
+                System.out.println(getSnowflakeId());
+            }
+        } catch (Exception e) {
+
+        }
+        System.out.println("10万耗时: " + (System.currentTimeMillis()-start) + "ms");
+
+    }
+}
